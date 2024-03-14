125f46b1081f558794f98b59c26f13a62a73377e
xoFrey <xizelacar@gmail.com>
2024-03-14 11:32:44 +0100

    psu

--- src/components/Foodrotation/Foodrotation.jsx
+++ src/components/Foodrotation/Foodrotation.jsx
@@ -5,22 +5,22 @@ const Foodrotation = () => {
     <section className="rotation">
       <div className="circle">
         <div className="chicken">
-          <img src="../../../public/img/chicken.svg" alt="" />
+          <img src="/img/chicken.svg" alt="" />
         </div>
         <div className="hotdog">
-          <img src="../../../public/img/hotdog.svg" alt="" />
+          <img src="/img/hotdog.svg" alt="" />
         </div>
         <div className="raddish">
-          <img src="../../../public/img/raddish.svg" alt="" />
+          <img src="/img/raddish.svg" alt="" />
         </div>
         <div className="salad">
-          <img src="../../../public/img/salad.svg" alt="" />
+          <img src="/img/salad.svg" alt="" />
         </div>
         <div className="onion">
-          <img src="../../../public/img/springonion.svg" alt="" />
+          <img src="/img/springonion.svg" alt="" />
         </div>
         <div className="sushi">
-          <img src="../../../public/img/sushi.svg" alt="" />
+          <img src="/img/sushi.svg" alt="" />
         </div>
       </div>
     </section>
