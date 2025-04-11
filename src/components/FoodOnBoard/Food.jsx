43875a12ac6b148cadd687ca48a425caab880c49
xofrey <izel.acxr@gmail.com>
2025-04-11 12:31:23 +0200

    removed public

--- src/components/FoodOnBoard/Food.jsx
+++ src/components/FoodOnBoard/Food.jsx
@@ -2,13 +2,31 @@ import "./Food.css";
 
 const Test = () => {
   return (
-    <div className="food">
-      <img src="../../../public/img/chicken.svg" alt="" />
-      <img src="../../../public/img/hotdog.svg" alt="" />
-      <img src="../../../public/img/raddish.svg" alt="" />
-      <img src="../../../public/img/salad.svg" alt="" />
-      <img src="../../../public/img/springonion.svg" alt="" />
-      <img src="../../../public/img/sushi.svg" alt="" />
+    <div className='food'>
+      <img
+        src='../img/chicken.svg'
+        alt=''
+      />
+      <img
+        src='../img/hotdog.svg'
+        alt=''
+      />
+      <img
+        src='../img/raddish.svg'
+        alt=''
+      />
+      <img
+        src='../img/salad.svg'
+        alt=''
+      />
+      <img
+        src='../img/springonion.svg'
+        alt=''
+      />
+      <img
+        src='../img/sushi.svg'
+        alt=''
+      />
     </div>
   );
 };
